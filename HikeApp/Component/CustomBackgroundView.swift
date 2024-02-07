8358a2850d54ae7c508ba545cd6cd0ea8e59a167
itscacv <carloscarventemt@gmail.com>
2024-02-07 17:55:52 -0600

    CardView added

---
+++ HikeApp/Component/CustomBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomBackgroundView.swift
+//  HikeApp
+//
+//  Created by carlos velasco on 2/7/24.
+//
+
+import SwiftUI
+
+struct CustomBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomBackgroundView()
+}
---
+++ HikeApp/Utility/ColorExtension.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtension.swift
+//  HikeApp
+//
+//  Created by carlos velasco on 2/7/24.
+//
+
+import Foundation
