79bac1066b57524f687cc5d8e0671ee7f7f2ce59
itscacv <carloscarventemt@gmail.com>
2024-02-08 16:03:24 -0600

    Add Color extension

--- HikeApp/Component/CardView.swift
+++ HikeApp/Component/CardView.swift
@@ -10,22 +10,27 @@ import SwiftUI
 struct CardView: View {
     var body: some View {
         ZStack {
-            Circle()
-                .fill(
-                    LinearGradient(
-                        colors: [
-                            Color("ColorIndigoMedium"),
-                            Color("ColorSalmonLight")
-                        ], 
-                        startPoint: .topLeading,
-                        endPoint: .bottomTrailing)
-                )
-                .frame(width: 256, height: 256)
+            CustomBackgroundView()
             
-            Image("image-1")
-                .resizable()
-            .scaledToFit()
-        }
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                            ], 
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing)
+                    )
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                    .resizable()
+                .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
