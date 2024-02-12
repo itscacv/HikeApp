66fe8405eda619b7ba4db429046c1c35c69ce191
itscacv <carloscarventemt@gmail.com>
2024-02-12 13:13:39 -0600

    Add CustomCircleView

---
+++ HikeApp/Component/CustomCircleView.swift
@@ -0,0 +1,34 @@
+//
+//  CustomCircleView.swift
+//  HikeApp
+//
+//  Created by carlos velasco on 2/9/24.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    @State private var isAnimatedGradient: Bool = false
+    var body: some View {
+        Circle()
+            .fill(
+                LinearGradient(
+                    colors: [
+                        .customIndigoMedium,
+                        .customSalmonLight
+                    ],
+                    startPoint: isAnimatedGradient ? .topLeading : .bottomLeading,
+                    endPoint: isAnimatedGradient ? .bottomTrailing : .topTrailing)
+            )
+            .onAppear {
+                withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+                    isAnimatedGradient.toggle()
+                }
+            }
+            .frame(width: 256, height: 256)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
